4f17e786344811e96b62a30b20dce7259954166f
smile159 <1174139226@qq.com>
2020-07-02 20:24:57 +0800

    提交基本的结构

---
+++ src/main/java/com/schq/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.schq.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * \* Desc:
+ * \* User: HQ
+ * \* Date: 2020/7/2  20:22
+ * \
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello Git!";
+    }
+}
